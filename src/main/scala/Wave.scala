29527808d37a3c21dc6040046f107d4a2b0b033e
Michael <michael.socha.99@hotmail.ca>
2026-04-20 16:42:16 -0700

    add more verticle variance in enemy positions and add 3rd wave

--- src/main/scala/Wave.scala
+++ src/main/scala/Wave.scala
@@ -3,15 +3,23 @@ case class Wave(enemies: Seq[Enemy])
 object Waves:
   val all: Seq[Wave] = Seq(
     Wave(Seq(
-      new BasicEnemy(150, 100),
-      new BasicEnemy(400, 100),
-      new BasicEnemy(650, 100)
+      new BasicEnemy(150,  40),
+      new BasicEnemy(400, 200),
+      new BasicEnemy(650,  80)
     )),
     Wave(Seq(
-      new BasicEnemy(100, 80),
-      new BasicEnemy(250, 80),
-      new BasicEnemy(400, 80),
-      new BasicEnemy(550, 80),
-      new BasicEnemy(700, 80)
-    ))
+      new BasicEnemy(100,  30),
+      new BasicEnemy(250, 250),
+      new BasicEnemy(400,  90),
+      new BasicEnemy(550, 320),
+      new BasicEnemy(700,  50)
+    )),
+    Wave(Seq(
+      new BasicEnemy( 80, 300),
+      new BasicEnemy(220,  50),
+      new BasicEnemy(360, 200),
+      new BasicEnemy(500,  30),
+      new BasicEnemy(640, 280),
+      new BasicEnemy(750, 120)
+    )),
   )
